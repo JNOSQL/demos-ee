9373071dc7295d5937df099dbd372fde8d5722cc
Maximillian Arruda <dearrudam@gmail.com>
2023-03-03 19:24:16 -0300

    chore: creates an liberty-mongodb module
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ liberty-mongodb/src/main/java/org/eclipse/jnosql/demoee/liberty/mongodb/config/ConfigTestController.java
@@ -0,0 +1,33 @@
+package org.eclipse.jnosql.demoee.liberty.mongodb.config;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/config")
+@RequestScoped
+public class ConfigTestController {
+
+    @Inject
+    @ConfigProperty(name = "injected.value")
+    private String injectedValue;
+
+    @Path("/injected")
+    @GET
+    public String getInjectedConfigValue() {
+        return "Config value as Injected by CDI " + injectedValue;
+    }
+
+    @Path("/lookup")
+    @GET
+    public String getLookupConfigValue() {
+        Config config = ConfigProvider.getConfig();
+        String value = config.getValue("value", String.class);
+        return "Config value from ConfigProvider " + value;
+    }
+}
