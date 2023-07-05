e6f57cf596e2cf452957780ee7824160deec04f8
Otavio Santana <otaviopolianasantana@gmail.com>
2023-07-05 21:07:07 +0100

    add fish service
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- quarkus-mongodb/src/main/java/org/jnosql/demoee/FishService.java
+++ quarkus-mongodb/src/main/java/org/jnosql/demoee/FishService.java
@@ -12,6 +12,7 @@ import org.bson.types.ObjectId;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class FishService {
@@ -27,20 +28,22 @@ public class FishService {
     }
 
     public Fish insert(Fish fish) {
+        fish.id = UUID.randomUUID().toString();
         return this.template.insert(fish);
     }
 
     public void delete(String id) {
-        this.template.delete(Fish.class, new ObjectId(id));
+        this.template.delete(Fish.class, id);
     }
 
     public Optional<Fish> findById(String id){
-        return this.template.find(Fish.class, new ObjectId(id));
+        return this.template.find(Fish.class, id);
     }
 
     public Fish random() {
         Fish fish = new Fish();
         Animal animal = faker.animal();
+        fish.id = UUID.randomUUID().toString();
         fish.name = animal.name();
         fish.color = faker.color().name();
         return this.template.insert(fish);
