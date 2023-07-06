36d30ca632eb115c54820e74f9446345a3210ace
Otavio Santana <otaviopolianasantana@gmail.com>
2023-07-06 10:20:54 +0100

    chore: include payara version
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ payara-mongodb/src/main/java/org/jnosql/demoee/payara/mongodb/HelloController.java
@@ -0,0 +1,18 @@
+package org.jnosql.demoee.payara.mongodb;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
