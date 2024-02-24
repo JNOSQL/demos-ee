1009ce3cef1028bef939764e031b9dae49f466b2
Otavio Santana <otaviopolianasantana@gmail.com>
2024-02-24 13:09:19 +0000

    feat: extension to beer repository with quarkus
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- quarkus-jnosql-oracle-nosql/src/main/java/org/acme/BeerRepository.java
+++ quarkus-jnosql-oracle-nosql/src/main/java/org/acme/BeerRepository.java
@@ -14,17 +14,12 @@
  */
 package org.acme;
 
-import jakarta.data.repository.PageableRepository;
-import jakarta.data.repository.Query;
+import jakarta.data.repository.BasicRepository;
 import jakarta.data.repository.Repository;
 
 import java.util.Set;
 
 @Repository
-public interface BeerRepository extends PageableRepository<Beer, String> {
+public interface BeerRepository extends BasicRepository<Beer, String> {
 
-    Set<Beer> findByStyle(String style);
-
-    @Query("select * from Beer")
-    Set<Beer> query();
 }
