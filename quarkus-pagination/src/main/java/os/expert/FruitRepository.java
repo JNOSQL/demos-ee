20025d158a38b7052273492c2e27f0e79dc9ebfc
Otavio Santana <otaviopolianasantana@gmail.com>
2024-04-15 17:45:50 +0100

    feat: update structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- quarkus-pagination/src/main/java/os/expert/FruitRepository.java
+++ quarkus-pagination/src/main/java/os/expert/FruitRepository.java
@@ -4,13 +4,13 @@ import jakarta.data.Sort;
 import jakarta.data.page.CursoredPage;
 import jakarta.data.page.Page;
 import jakarta.data.page.PageRequest;
+import jakarta.data.repository.BasicRepository;
 import jakarta.data.repository.Find;
 import jakarta.data.repository.OrderBy;
 import jakarta.data.repository.Repository;
-import org.eclipse.jnosql.mapping.NoSQLRepository;
 
 @Repository
-public interface FruitRepository extends NoSQLRepository<Fruit, String> {
+public interface FruitRepository extends BasicRepository<Fruit, String> {
 
     @Find
     CursoredPage<Fruit> cursor(PageRequest pageRequest, Sort<Fruit> order);
